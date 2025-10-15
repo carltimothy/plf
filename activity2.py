491809fbf8e92508bb0fafa7e2253e70eab95449
Carl Timothy Ligutom <carl.timothy.ligutom@student.uno-r.edu.ph>
2025-10-15 20:09:05 +0800

    Add comma format on balance
    * It looks stupid without it so I fixed it.

--- activity2.py
+++ activity2.py
@@ -15,19 +15,19 @@ else:
         menu = int(input("Choose an option: "))
         if menu == 1:
             print(f"Account Holder: {user}")
-            print(f"Current Balance: ₱{balance}", "\n")
+            print(f"Current Balance: ₱{balance:,}", "\n")
         elif menu == 2:
             amount = int(input("Enter amount to deposit: "))
             if amount == 100 or amount == 200 or amount == 500 or amount == 1000:
                 balance += amount
-                print(f"You deposited ₱{amount}. New Balance: ₱{balance}\n")
+                print(f"You deposited ₱{amount}. New Balance: ₱{balance:,}\n")
             else:
                 print("Invalid input. Can only deposit ₱100, ₱200, ₱500, ₱1000", "\n")
         elif menu == 3:
             withdraw = int(input("Enter amount to withdraw: "))
             if withdraw == 100 or withdraw == 200 or withdraw == 500 or withdraw == 1000:
                 balance -= withdraw
-                print(f"You took ₱{withdraw}. New Balance: ₱{balance}\n")
+                print(f"You took ₱{withdraw}. New Balance: ₱{balance:,}\n")
             elif withdraw > balance:
                 print("Insufficient Balance!") 
             else:
