9d6531e3c886aa3c2dea2e82bcc837bec4701ca5
Carl Timothy Ligutom <carl.timothy.ligutom@student.uno-r.edu.ph>
2025-10-21 07:37:53 +0000

    Refactor: Rename row/col variables to rows/columns for clarity

--- activity nanaman.py
+++ activity nanaman.py
@@ -1,8 +1,8 @@
-row = int(input("No. of rows: "))
-col = int(input("No. of columns: "))
+rows = int(input("No. of rows: "))
+columns = int(input("No. of columns: "))
 
-for r in range(1, row + 1):
-    for c in range(1, col + 1):
+for r in range(1, rows + 1):
+    for c in range(1, columns + 1):
         print(f"{r * c:6}", end=" ")
     print()
     # What I did here is that first nag initiate to multiply row and column then add spacing inbetween
